2996005d5997277a6ff38ba3dec1978d633fca25
Tajinder1993 <ktajindershahkot93@gmail.com>
2025-03-30 00:59:54 -0400

    Working with the codegen in playwright

---
+++ tests/Code_with_Codegen.spec.js
@@ -0,0 +1,47 @@
+/*
+This code is generated with the codegen.
+copy paste the code from codegen playwright inspector.
+The command to generate the code is :
+
+npx playwright codegen 
+*/
+
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+
+    // fetch the correct URL
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+
+    //click on user name text box code to be deleted in final code
+    //this step serves no coding purpose
+    await page.getByRole('textbox', { name: 'Username' }).click();
+
+    // fill the user name 'Admin'
+    await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+
+    //click on password text box code to be deleted in final code
+    //this step serves no coding purpose
+    await page.getByRole('textbox', { name: 'Password' }).click();
+
+    // Fill the password 'admin123'
+    await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+
+    // click on the login button
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    // open the drop down menu. click on the arrow
+    await page.locator('span').filter({ hasText: 'manda user' }).click();
+
+    // click on the logout button
+    await page.getByRole('menuitem', { name: 'Logout' }).click();
+});
+
+/*
+
+run the code
+---------------------------------------
+npx playwright test Code_with_Codegen.spec.js
+
+
+*/
