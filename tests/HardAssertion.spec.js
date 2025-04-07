9f97791da70f6a5ce06479d8dd09d57bd86c4ff1
Tajinder1993 <ktajindershahkot93@gmail.com>
2025-04-06 21:37:53 -0400

    hard assertions day 2

--- tests/HardAssertion.spec.js
+++ tests/HardAssertion.spec.js
@@ -31,19 +31,56 @@ test('HardAssertions', async ({ page }) => {
     //5. verify the input box is enable
     const firstName = await page.locator('#FirstName')
     await expect(firstName).toBeEnabled();
-    await page.waitForTimeout(2000);
+    await page.waitForTimeout(1000);
     await page.locator('//input[@id="FirstName"]').fill('Tajinder');
 
     // fill the last name
-    await page.waitForTimeout(2000)
+    await page.waitForTimeout(1000)
     await page.locator('//input[@id="LastName"]').fill('Kaur')
 
     // fill the email
-    await page.waitForTimeout(2000)
+    await page.waitForTimeout(1000)
     await page.locator('//input[@id="Email"]').fill('tajdemo@gmail.com')
 
+    //6. verify the company detail title with the .toHaveText() assertion
     const test1 = await page.locator("//strong[normalize-space()='Company Details']")
     await expect(test1).toHaveText('Company Details')
+
+    // fill the compnay information 
+    await page.locator('#Company').fill("Bethink Computer Institute");
+    await page.waitForTimeout(1000)
+
+    //uncheck the checkbox
+    await page.locator('#Newsletter').click();
+    await page.waitForTimeout(1000)
+
+    // verfiy the check is not checked 
+    const checkBox = await page.locator('#Newsletter')
+    await expect(checkBox).not.toBeChecked();
+
+    //check the check box
+    await page.locator('#Newsletter').click();
+
+    //enter the password
+    const pass = await page.locator('#Password').fill("Taj123")
+
+    //Renter the password
+    const rePass = await page.locator('#ConfirmPassword').fill('Taj123')
+
+    //validate the password
+    await page.waitForTimeout(2000)
+    await expect(rePass).toEqual(pass)
+
+    // verify that register button type is submit
+    const registerButton = await page.locator('#register-button')
+    await expect(registerButton).toHaveAttribute('type', 'submit');
+
+    //hover the mouse on register button
+    await page.waitForTimeout(2000)
+    await page.locator('#register-button').hover();
+    await page.waitForTimeout(2000)
+
+
 
 
 
